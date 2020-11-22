e484d3f9a294c5f40c9fb9e2147b900c6bd78e63
iColtz <max.hurst@hotmail.com>
2020-11-22 20:59:50 +0000

    bot(test pages)

---
+++ test/bot.js
@@ -0,0 +1,18 @@
+const { token } = require("./config.js");
+const { Client, MessageEmbed } = require("discord.js");
+const EmbedPages = require("../src/index.js");
+const client = new Client();
+
+client.once("ready", () => console.log("Yoo this is ready!"));
+
+client.on("message", async (message) => {
+    if (message.content.startsWith("?test")) {
+        const embed1 = new MessageEmbed().setColor("RED").setDescription("Test Number 1");
+        const embed2 = new MessageEmbed().setColor("BLUE").setDescription("Test Number 2");
+        const embed3 = new MessageEmbed().setColor("YELLOW").setDescription("Test Number 3");
+        const pages = [embed1, embed2, embed3];
+        new EmbedPages(pages, message);
+    }
+});
+
+client.login(token);
