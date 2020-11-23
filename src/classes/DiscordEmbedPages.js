efed782184a34723fc8a1788a2e93f03e0086e4d
iColtz <max.hurst@hotmail.com>
2020-11-23 18:53:30 +0000

    DiscordEmbedPages(comments)

--- src/classes/DiscordEmbedPages.js
+++ src/classes/DiscordEmbedPages.js
@@ -1,6 +1,20 @@
 const { TextChannel, MessageEmbed } = require("discord.js");
 
+/**
+ * Options used to determine how to embed pages should be constructed.
+ * @typedef {Object} PagesOptions
+ * @prop {Array} pages - An array of message embed that will be in the embed pages.
+ * @prop {Discord.TextChannel} channel - The channel the embed pages will be sent.
+ * @prop {Number} [duration=60000] - The length the reaction collector will last.
+ * @prop {Array<Snowflake>|String<Snowflake>|Function} - The restricted users to the embed pages.
+ * @prop {Boolean} - Whether or not to have the page counter on the embed footer.
+ */
+
 class DiscordEmbedPages {
+    /**
+     * Created the embed pages.
+     * @param {PagesOptions} options - Options for the embed pages. 
+     */
     constructor({
         pages,
         channel,
@@ -8,21 +22,48 @@ class DiscordEmbedPages {
         restricted,
         pageFooter,
     } = {}) {
+        /**
+         * List of pages for the embed pages.
+         * @type {Array<Discord.MessageEmbed>}
+         */
         this.pages = pages;
 
+        /**
+         * Channel to send the embed pages to.
+         * @type {Discord.TextChannel}
+         */
         this.channel = channel;
 
+        /**
+         * How long the reactions collector will last in milliseconds.
+         * @type {Number}
+         */
         this.duration = duration || 60000;
 
+        /**
+         * Only user's that can use the embed reactions.
+         * @type {Array<Snowflake>|String<Snowflake>|Function}
+         */
         this.restricted = restricted;
 
+        /**
+         * Whether to have a page counter on the embed footers.
+         * @type {Boolean}
+         */
         this.pageFooter = Boolean(pageFooter);
 
+        /**
+         * The current page number to embed pages is on.
+         * @type {Number}
+         */
         this.currentPageNumber = 0;
 
         this.validate();
     }
 
+    /**
+     * Creates and sends the embed pages.
+     */
     createPages() {
         if (!this.pages[0]) throw new Error("Tried to create embed pages with no pages in the pages array.");
         if (this.pageFooter) this.pages[0].setFooter(`Page: 1/${this.pages.length}`);
@@ -52,6 +93,9 @@ class DiscordEmbedPages {
         });
     }
 
+    /**
+     * Turns the embed pages to the next page.
+     */
     nextPage() {
         if (!this.msg) throw new Error("Tried to go to next page but embed pages havn't been created yet.");
         this.currentPageNumber++;
@@ -61,6 +105,9 @@ class DiscordEmbedPages {
         this.msg.edit({ embed: embed }).catch(() => null);
     }
 
+    /**
+     * Turns the embde pages to the previous page.
+     */
     previousPage() {
         if (!this.msg) throw new Error("Tried to go to previous page but embed pages havn't been created yet.");
         this.currentPageNumber--;
@@ -70,6 +117,10 @@ class DiscordEmbedPages {
         this.msg.edit({ embed: embed }).catch(() => null);
     }
 
+    /**
+     * Adds a page to the embed pages.
+     * @param {Discord.MessageEmbed} embed - Embed that is added to the embed pages.
+     */
     addPage(embed) {
         if (!this.msg) throw new Error("Tried to add page before embed pages have even been created.");
         if (!(embed instanceof MessageEmbed)) throw new Error("Adding embed is not a instance of a message embed.");
@@ -79,6 +130,10 @@ class DiscordEmbedPages {
         this.msg.edit({ embed: currentEmbed });
     }
 
+    /**
+     * Removes a page from the embed pages.
+     * @param {Number} pageNumber - The page index that is removed.
+     */
     deletePage(pageNumber) {
         if (!this.msg) throw new Error("Tried to delete page before embed pages have even been created.");
         if (pageNumber < 0 || pageNumber > this.pages.length - 1) throw new Error("Deleting page does not exist.");
@@ -97,6 +152,10 @@ class DiscordEmbedPages {
         }
     }
 
+    /**
+     * Turns the embed pages to a certain page.
+     * @param {Number} pageNumber - The page index that is turned to.
+     */
     turnToPage(pageNumber) {
         if (!this.msg) throw new Error("Tried to turn to page before embed pages have even been created.");
         if (pageNumber < 0 || pageNumber > this.pages.length - 1) throw new Error("Turning page does not exist.");
@@ -106,11 +165,17 @@ class DiscordEmbedPages {
         this.msg.edit({ embed: embed }).catch(() => null);
     }
 
+    /**
+     * Deletes the embed pages.
+     */
     delete() {
         if (!this.msg) throw new Error("Tried to delete embed pages but they havn't even been created yet.");
         this.msg.delete().catch(() => null);
     }
 
+    /**
+     * Validates the embed pages options are correct.
+     */
     validate() {
         if (!Array.isArray(this.pages)) {
             throw new Error("Pages option needs to be an array.");
